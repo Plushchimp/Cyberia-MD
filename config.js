df91a634881ddc114ff4514b870e699e3c8a0294
Plushchimp <88704487+Plushchimp@users.noreply.github.com>
2025-08-06 02:12:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  SESSION_ID: "drakonis~sj8jzbhZ#qII8GbCumdTWMthxSM0gpG-WmbqumSZFYYI6uYztgFU",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "1(876)707-7170", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
